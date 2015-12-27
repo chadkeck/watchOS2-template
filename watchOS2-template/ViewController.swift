904aade447493a26ed615c07609b8315367bc063
Chad Bibler <chad@chadbibler.com>
2015-12-27 16:25:33 -0600

    Add Xcode project

---
+++ watchOS2-template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchOS2-template
+//
+//  Created by Chad Bibler on 12/27/15.
+//  Copyright © 2015 Chad Bibler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
